5f6b4511027dfe3a5afad8d522afcc1630d7b9db
Xymanek <xymanek@outlook.com>
2020-03-07 23:19:55 +0200

    Proper discord handler bot + wire up the github stuff

--- DiscordGitToolbox.App/Program.cs
+++ DiscordGitToolbox.App/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using AutoMapper;
-using Discord;
 using Discord.WebSocket;
 using DiscordGitToolbox.Core.ItemMention;
 using DiscordGitToolbox.GitHub;
@@ -13,52 +12,44 @@ namespace DiscordGitToolbox.App
 {
     internal static class Program
     {
-        private static async Task Main()
+        private static void Main()
         {
             Console.WriteLine("Hello World!");
 
-            await Discord();
+            // await Discord();
 
             IServiceCollection serviceCollection = new ServiceCollection();
             ConfigureServices(serviceCollection);
             ServiceProvider container = serviceCollection.BuildServiceProvider();
 
-            IMentionPipeline pipeline = container.GetRequiredService<IMentionPipeline>();
+            var discordHandler = container.GetRequiredService<DiscordEventsHandler>();
+
+            discordHandler.RegisterHandlers();
+
+            Task worker = Task.Factory.StartNew(() => discordHandler.ConnectAndWork(), TaskCreationOptions.LongRunning);
+            // worker.Wait();
+
+            Console.ReadLine();
+
+            /*IMentionPipeline pipeline = container.GetRequiredService<IMentionPipeline>();
             
             foreach (string s in await pipeline.GetLinksForMessage("ci#123 chl#456"))
             {
                 Console.WriteLine(s);                
-            }
-        }
-        
-        private static DiscordSocketClient _client;
-
-        private static async Task Discord()
-        {
-            _client = new DiscordSocketClient();
-            
-            _client.Log += Log;
-            _client.MessageReceived += MessageReceived;
-            
-            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken"));
-            await _client.StartAsync();
-
-            // Block this task until the program is closed.
-            await Task.Delay(-1);
-        }
-        
-        private static async Task MessageReceived(SocketMessage message)
-        {
-            if (message.Content == "!ping")
-            {
-                await message.Channel.SendMessageAsync("Pong!");
-            }
+            }*/
         }
 
         private static void ConfigureServices(IServiceCollection serviceCollection)
         {
-            serviceCollection.AddSingleton<IMentionPipeline, MentionPipeline>();
-            
+            // Discord client
+            serviceCollection.AddSingleton<DiscordSocketClient>();
+            serviceCollection.AddSingleton<BaseSocketClient>(
+                provider => provider.GetRequiredService<DiscordSocketClient>()
+            );
+
+            // Discord handler
+            serviceCollection.AddSingleton<DiscordEventsHandler>();
+
             // Github client config
             serviceCollection.AddSingleton(
                 provider => new GitHubClientCollection(
@@ -68,11 +59,11 @@ namespace DiscordGitToolbox.App
             serviceCollection.AddSingleton<IGitHubClientResolver>(
                 provider => provider.GetRequiredService<GitHubClientCollection>()
             );
-            
+
             // Github mentions config
             serviceCollection.AddSingleton(provider => new GitHubMentionConfiguration
             {
-                Repositories = new []
+                Repositories = new[]
                 {
                     new GitHubMentionConfiguration.Repository
                     {
@@ -91,20 +82,17 @@ namespace DiscordGitToolbox.App
                 }
             });
 
+            // Mentions pipeline
+            serviceCollection.AddSingleton<IMentionPipeline, MentionPipeline>();
+
             // Mention resolvers
             serviceCollection.AddSingleton<IMentionResolver, GitHubMentionResolver>();
-            
+
             // Mention extractors
             serviceCollection.AddSingleton<IMentionExtractor, GitHubMentionExtractor>();
-            
+
             // Automapper
             serviceCollection.AddAutoMapper(typeof(GitHubMapperProfile));
-        }
-        
-        private static Task Log(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
         }
     }
 }
