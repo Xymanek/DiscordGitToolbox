dafa9af701d91f79a622565543e22c503717d02f
Xymanek <xymanek@outlook.com>
2020-03-07 15:49:16 +0200

    Basic github API usage

--- DiscordGitToolbox.App/Program.cs
+++ DiscordGitToolbox.App/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Octokit;
 
 namespace DiscordGitToolbox.App
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            var github = new GitHubClient(new ProductHeaderValue("DiscordGitToolboxApp"));
+
+            PullRequest pr = await github.PullRequest.Get("WOTCStrategyOverhaul", "CovertInfiltration", 458);
+            Console.WriteLine("PR: " + pr.Title + " " + pr.HtmlUrl);
+
+            try
+            {
+                pr = await github.PullRequest.Get("WOTCStrategyOverhaul", "CovertInfiltration", 456);
+                Console.WriteLine("PR: " + pr.Title + " " + pr.HtmlUrl);
+            }
+            catch (NotFoundException)
+            {
+                Console.WriteLine("PR #456 not found");
+                // throw;
+            }
         }
     }
 }
