d47e83bb5b8d8d62795521ffe5d61ee0d8f3e67f
Xymanek <xymanek@outlook.com>
2020-03-07 15:30:40 +0200

    Initialized project

---
+++ DiscordGitToolbox.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordGitToolbox.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
