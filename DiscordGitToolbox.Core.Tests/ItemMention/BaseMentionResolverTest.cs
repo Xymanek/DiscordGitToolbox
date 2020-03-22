611cc352cf17c56a42a2cf11cdb99f5d3ce4b5a3
Xymanek <xymanek@outlook.com>
2020-03-22 17:51:22 +0200

    Added tests for BaseMentionResolverTest

---
+++ DiscordGitToolbox.Core.Tests/ItemMention/BaseMentionResolverTest.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using DiscordGitToolbox.Core.ItemMention;
+using Moq;
+using Xunit;
+
+namespace DiscordGitToolbox.Core.Tests.ItemMention
+{
+    public class BaseMentionResolverTest
+    {
+        [Fact]
+        public async Task TestUnsupportedMentionException()
+        {
+            var invalidMention = new Mock<IMention>().Object;
+            var resolver = new EmptyMentionResolver();
+
+            UnsupportedMentionException exception = await Assert.ThrowsAsync<UnsupportedMentionException>(
+                () => resolver.ResolveMentionAsync(invalidMention)
+            );
+            Assert.Same(invalidMention, exception.Mention);
+        }
+
+        [Fact]
+        public void TestIsSupported()
+        {
+            var resolver = new EmptyMentionResolver();
+
+            var validMention = new EmptyMention();
+            var invalidMention = new Mock<IMention>().Object;
+
+            Assert.True(resolver.IsMentionSupported(validMention));
+            Assert.False(resolver.IsMentionSupported(invalidMention));
+        }
+
+        private class EmptyMention : IMention
+        {
+        }
+
+        private class EmptyMentionResolver : BaseMentionResolver<EmptyMention>
+        {
+            public override Task<IReference?> ResolveMentionAsync(EmptyMention mention)
+            {
+                return Task.FromResult<IReference?>(null);
+            }
+        }
+    }
+}
--- DiscordGitToolbox.Core/ItemMention/IMentionResolver.cs
+++ DiscordGitToolbox.Core/ItemMention/IMentionResolver.cs
@@ -5,7 +5,7 @@ namespace DiscordGitToolbox.Core.ItemMention
     public interface IMentionResolver
     {
         bool IsMentionSupported(IMention mention);
-        
+
         /// <exception cref="UnsupportedMentionException">If IsMentionSupported returned false</exception>
         Task<IReference?> ResolveMentionAsync(IMention mention);
     }
@@ -14,5 +14,26 @@ namespace DiscordGitToolbox.Core.ItemMention
         where TMention : IMention
     {
         Task<IReference?> ResolveMentionAsync(TMention mention);
+    }
+
+    public abstract class BaseMentionResolver<TMention> : IMentionResolver<TMention>
+        where TMention : IMention
+    {
+        public bool IsMentionSupported(IMention mention)
+        {
+            return mention is TMention;
+        }
+
+        public Task<IReference?> ResolveMentionAsync(IMention mention)
+        {
+            if (!IsMentionSupported(mention))
+            {
+                throw new UnsupportedMentionException(mention);
+            }
+
+            return ResolveMentionAsync((TMention) mention);
+        }
+
+        public abstract Task<IReference?> ResolveMentionAsync(TMention mention);
     }
 }
